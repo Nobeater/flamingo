cb7aaf20ba71df3c9f5fc629144765d3839bcd75
Nobeater <tyler.dechaine-whiteis@mnsu.edu>
2014-10-17 11:41:34 -0500

    Clean up

--- h.sh
+++
@@ -1,6 +0,0 @@
-1#!/bin/bash/
-# i dont know
-
-echo "hello world!"
-echo "Here is a list of your stuff"
-exec top
--- lanch-tor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/
-# This will launch tor
-
-echo "Opening Tor"
-cd /home/leo/Applications/tor
-exec ./st*
